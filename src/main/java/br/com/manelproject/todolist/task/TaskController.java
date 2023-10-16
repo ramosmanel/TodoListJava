87385687286621d00b6ee3d2690762195457df38
unknown <emanuelramos20004@gmail.com>
2023-10-16 19:48:24 -0300

    Dockerfile deploy

--- src/main/java/br/com/manelproject/todolist/task/TaskController.java
+++ src/main/java/br/com/manelproject/todolist/task/TaskController.java
@@ -52,10 +52,22 @@ public class TaskController {
 
     //http:localhost:8080/tasks/{idtask}
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada!");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O usuário não tem permissão para alterar essa tarefa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
     }
     
 }
